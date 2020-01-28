5b9fc30a903de80d3adaab14943df3c3f4bac5b3
sezzerouali <sami.ezzerouali@amadeus.com>
2020-01-28 12:21:25 +0100

    test case for makefile

--- include/readDb.hpp
+++ include/readDb.hpp
@@ -3,7 +3,9 @@
 class database {      
     std::string source;       
   public:         
+    database();
     std::string myString; 
     int setSource(std::string _source);
+    std::string getSource();
     
 };
--- src/readDb.cpp
+++ src/readDb.cpp
@@ -1,10 +1,16 @@
-#include "../include/readDb.hpp"
+#include <iostream>
+#include "readDb.hpp"
 
-database::database(){
-
+database::database(void){
+    source="./db/";
+    std::cout << "db created\n";
 }
 
 int database::setSource(std::string _source){
     source=_source;
     return 1;
+}
+
+std::string database::getSource(){
+    return source;
 }
