a6f4d7dbdd362095a02aaddd66b5efa5351f5e40
sezzerouali <sami.ezzerouali@amadeus.com>
2020-01-28 12:21:10 +0100

    simple test for makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
-#include "./include/readDb.hpp"
+#include <iostream>
+#include "readDb.hpp"
 
 
 int main(int argc, char const *argv[])
 {
-    
+    database db;
+    db.setSource("./db/users.json");
+    std::cout << "Source : " << db.getSource() << std::endl;
     return 0;
 }
