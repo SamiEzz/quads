788c7ba85a19eb5eaf1d9a45f880efa2c2fc706c
sezzerouali <sami.ezzerouali@amadeus.com>
2020-01-28 13:26:36 +0100

    clang

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
+
 #include "readDb.hpp"
+#include <iostream>
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    
-    database db;
-    db.setSource("./db/users.json");
-    cout << "Source : " << db.getSource() << endl;
-    return 0;
+int main(int argc, char const *argv[]) {
+  database db;
+  db.setSource("./db/users.json");
+  cout << "Source : " << db.getSource() << endl;
+  return 0;
 }
--- src/readDb.cpp
+++ src/readDb.cpp
@@ -1,15 +1,13 @@
 #include "readDb.hpp"
 
-database::database(void){
-    source="./db/";
-    std::cout << "db created\n";
+database::database(void) {
+  source = "./db/";
+  std::cout << "db created\n";
 }
 
-int database::setSource(std::string _source){
-    source=_source;
-    return 1;
+int database::setSource(std::string _source) {
+  source = _source;
+  return 1;
 }
 
-std::string database::getSource(){
-    return source;
-}
+std::string database::getSource() { return source; }
